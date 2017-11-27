a759f411533c334f793d1b61260f07bcbe66018e
Knut Melvær <knut.melvaer@netlife.com>
2017-11-27 22:25:11 +0100

    clean up and add message to root

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 "use strict";
 require("dotenv").config();
-
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  // application specific logging, throwing an error, or other logic here
+});
 const Path = require('path');
 const Hapi = require("hapi");
 const Inert = require('inert');
 
-const {
-  favicon
-} = require('./lib/plugins/')
+const podcastRSS = require('./lib/handlers/podcastRss');
+const { favicon } = require('./lib/plugins/')
+
 const server = new Hapi.Server({
   port: process.env.PORT || 8888,
   routes: {
@@ -16,22 +19,31 @@ const server = new Hapi.Server({
     }
   },
   plugins: {
-
     favicon
   },
-
 });
+
 const provision = async () => {
-const podcastRSS = require('./lib/handlers/podcastRss');
   // Add the route
   await server.register(Inert)
+
   server.route({
     method: 'GET',
-    path: '/{path*}',
+    path: '/',
+    options: {
+      handler: async (request, h) => {
+        return 'Welcome to Sanity Podcast Server.'
+      }
+    }
+    });
+
+  server.route({
+    method: 'GET',
+    path: '/favicon.ico',
     handler: {
         file: 'favicon.ico'
-    }
-});
+        }
+    });
 
   server.route({
     method: 'GET',
