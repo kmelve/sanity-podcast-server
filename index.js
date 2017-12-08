4c858b6c472ace4d830f32c1e2fef0e666c9b439
Knut Melvær <knut.melvaer@netlife.com>
2017-12-08 22:26:17 +0100

    clean up and register Inert

--- index.js
+++ index.js
@@ -1,65 +1,38 @@
-"use strict";
-require("dotenv").config();
+'use strict';
+require('dotenv').config();
 process.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
   // application specific logging, throwing an error, or other logic here
 });
-const Path = require('path');
-const Hapi = require("hapi");
+
+const Hapi = require('hapi');
 const Inert = require('inert');
 
-const podcastRSS = require('./lib/handlers/podcastRss');
-const { favicon } = require('./lib/plugins/')
+const { files, podcast } = require('./lib/routes/');
+const { favicon } = require('./lib/plugins/');
 
 const server = new Hapi.Server({
-  port: process.env.PORT || 8888,
-  routes: {
-    files: {
-        relativeTo: Path.join(__dirname, 'static')
-    }
-  },
-  plugins: {
-    favicon
-  },
+  port: process.env.PORT || 8888
 });
 
 const provision = async () => {
   // Add the route
-  await server.register(Inert)
+  await server.register(Inert);
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    options: {
-      handler: async (request, h) => {
-        return 'Welcome to Sanity Podcast Server.'
-      }
-    }
-    });
-
-  server.route({
-    method: 'GET',
-    path: '/favicon.ico',
-    handler: {
-        file: 'favicon.ico'
-        }
-    });
-
-  server.route({
-    method: 'GET',
-    path: '/{projectId}/{dataset}/{slug}/rss',
-    options: {
-      handler: async (request, h) => podcastRSS(request, h)
-    }
-  })
-
+  server.route([files, podcast]);
 
   // Start the server
   try {
-   await server.start().then(() => console.log('Server running:', server.info));
-  }
-  catch (err) {
+    await server.register([
+      {
+        plugin: Inert
+      }
+    ]);
+    await server
+      .start()
+      .then(() => console.log('Server running:', server.info));
+  } catch (err) {
     console.log(err);
   }
-}
+};
 provision();
